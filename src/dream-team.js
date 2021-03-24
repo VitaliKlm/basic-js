dae1cf96ce4b2613bc49b75d57f9d159aabfd519
VitaliKlm <kv37813@gmail.com>
2021-03-24 13:12:23 +0300

    feat: implement dream-team algorithm

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,15 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  
+  const array = members.map(member => {
+    if (typeof(member) === 'string') {
+      return member.trim().split('')[0].toUpperCase();
+    }
+    else return '';
+  });
+  return array.sort().join('');
 };
