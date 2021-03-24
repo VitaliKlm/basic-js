1873407804da1bbea692a3d4a77752bf1f34fa3d
VitaliKlm <kv37813@gmail.com>
2021-03-24 15:50:31 +0300

    feat: implement what-season algorithm

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (typeof(date) !== 'object' ) {
+    return 'Unable to determine the time of year!';
+  }
+  const month = Number(date.getUTCMonth());
+  const season = Math.floor((month + 1) / 3);
+  if (season === 1) {
+    return 'spring';
+  } else if (season === 2) {
+    return 'summer';
+  } else if (season === 3) {
+    return 'fall';
+  } else return 'winter';
 };
