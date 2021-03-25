52d407689a9487e007bfa47d356e71a5a4e7c104
VitaliKlm <kv37813@gmail.com>
2021-03-25 10:12:34 +0300

    feat: implement transform-array algorithm

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,68 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let discardNext = false;
+  const transformedArr = arr.map((element, index, array) => {
+
+    if (element === '--discard-next') {
+      element = 'Deleted!';
+      if (index !== array.length - 1) {
+        discardNext = true;
+      }
+      return element;
+    }
+
+    else if (element === '--discard-prev') {
+      if (index === 0) {
+        element = 'Deleted!';
+      }
+      return element;
+    }
+
+    else if (element === '--double-next') { 
+      element = 'Deleted!';
+      if (index !== array.length - 1) {
+        element = array[index + 1];
+      }
+      return element;
+    }
+  
+    else if (element === '--double-prev') {
+      element = 'Deleted!';
+      // ! 2-nd condition checks if the previous element has been removed
+      if (array[index - 2] == '--discard-next') {
+        return element;
+      }
+      if (index !== 0) {
+        element = array[index - 1]; 
+      }
+      // ! leave '--double-prev' control sequence to apply it after map method
+      return element;
+    }
+
+    else if (discardNext === true) {
+      discardNext = false;
+      element = 'Deleted!';
+      return element;
+    }
+
+    else return element;
+  })
+
+  // ! make array from all indexes of '--discard-prev' elements
+  const discardPrevIndexes = [];
+  transformedArr.forEach((elem, index) => {
+    if (elem === '--discard-prev') {
+      discardPrevIndexes.push(index);
+    }
+  });
+
+  // ! make all '--discard-prev' elements and previous elements = 'Deleted!'
+  discardPrevIndexes.forEach((elemIndex) => {
+    transformedArr[elemIndex] = 'Deleted!';
+    transformedArr[elemIndex - 1] = 'Deleted!';
+  });
+  console.log(transformedArr);
+  // ! delete all 'Deleted!' elements from final array
+  return transformedArr.filter(elem => elem !== 'Deleted!');
 };
