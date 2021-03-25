3ba6a59323cd8e7bf4b8002722ab02798710e5c0
VitaliKlm <kv37813@gmail.com>
2021-03-25 14:20:07 +0300

    feat: implement extended-repeater algorithm

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  if(options.repeatTimes === undefined) {
+    options.repeatTimes = 1;
+  }
+  if(options.separator === undefined) {
+    options.separator = '+';
+  }
+  if(options.addition === undefined) {
+    options.addition = '';
+  }
+  if(options.additionSeparator === undefined) {
+    options.additionSeparator = '|';
+  }
+  if(options.additionRepeatTimes === undefined) {
+    options.additionRepeatTimes = 1;
+  }
+
+  const additionStr = (String(options.addition) + options.additionSeparator)
+    .repeat(options.additionRepeatTimes - 1) + String(options.addition);
+
+  const mainStr = String(str) + additionStr;
+
+  const output = (mainStr + options.separator)
+    .repeat(options.repeatTimes - 1) + mainStr;
+
+  return output;
 };
   
